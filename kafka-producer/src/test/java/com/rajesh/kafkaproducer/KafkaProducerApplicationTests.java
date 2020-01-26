a3c014fb023d962905b55a67e8cadba12b3b349b
Rajesh Borade <Rajesh Borade>
2020-01-26 13:09:16 +0530

    Adding test for producer

--- kafka-producer/src/test/java/com/rajesh/kafkaproducer/KafkaProducerApplicationTests.java
+++ kafka-producer/src/test/java/com/rajesh/kafkaproducer/KafkaProducerApplicationTests.java
@@ -1,13 +1,74 @@
 package com.rajesh.kafkaproducer;
 
+import static java.util.Collections.singleton;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+
+
 
 @SpringBootTest
+@EmbeddedKafka
+@TestInstance(Lifecycle.PER_CLASS)
 class KafkaProducerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	private static final String TOPIC = "domain-events";
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    private Consumer<String, String> consumer;
+
+
+    @BeforeAll
+    public void setUp() {
+        Map<String, Object> configs = new HashMap<>(KafkaTestUtils.consumerProps("consumer", "false", embeddedKafkaBroker));
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new StringDeserializer(), new StringDeserializer()).createConsumer();
+        consumer.subscribe(singleton(TOPIC));
+        consumer.poll(0);
+    }
+
+    @AfterAll
+    public void tearDown() {
+        consumer.close();
+    }
+    
+    @Test
+    public void kafkaSetup_withTopic_ensureSendMessageIsReceived() {
+        // Arrange
+        Map<String, Object> configs = new HashMap<>(KafkaTestUtils.producerProps(embeddedKafkaBroker));
+        Producer<String, String> producer = new DefaultKafkaProducerFactory<>(configs, new StringSerializer(), new StringSerializer()).createProducer();
+
+        // Act
+        producer.send(new ProducerRecord<>(TOPIC, "my-aggregate-id", "test"));
+        producer.flush();
+
+        // Assert
+        ConsumerRecord<String, String> singleRecord = KafkaTestUtils.getSingleRecord(consumer, TOPIC);
+        assertThat(singleRecord).isNotNull();
+        assertThat(singleRecord.key()).isEqualTo("my-aggregate-id");
+        assertThat(singleRecord.value()).isEqualTo("test");
+        System.out.println("*** test passed");
+    }
 
 }
